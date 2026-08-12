7b49224a50691df550def3adc087bc849492b4ac
nemontero08 <monteronicolas08@gmail.com>
2026-08-12 09:47:12 -0300

    commit bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,14 +29,6 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es-AR" className={nunito.variable}>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
